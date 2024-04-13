0b033976a6aedd8e6c587f9f581ecfa776c1020b
Heba_Mustafa🍒 <163648306+heba-mustafa59@users.noreply.github.com>
2024-04-13 14:02:44 +0300

    Add files via upload

---
+++ random.java
@@ -0,0 +1,17 @@
+package q1;
+
+import java.util.*;
+
+public class Q1 {//heba jalil
+
+     public static void main(String[] args) {
+         Scanner in = new Scanner(System.in);
+       
+         for (int i = 0; i < 5; i++) {
+             System.out.println((int)(Math.random()*100));
+         }
+     }
+
+       
+}
+
